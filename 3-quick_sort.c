b634cd800224f7238eef75ed3c538774d36b15af
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-21 16:15:23 +0200

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,32 +7,37 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2 || ! array)
+	if (size < 2 || !array)
 	{
 		return;
 	}
-	quick_sort_recur(array, 0, size -1, size);
+	quick_sort_recur(array, 0, size - 1, size);
 }
 /**
  * quick_sort_recur - sort int array recuersively
- * @arr: int array
+ * @array: int array
  * @low: first element index
  * @heigh: last element index
  * @size: size of array
 */
-void quick_sort_recur(int *array, int low, int heigh , size_t size)
+void quick_sort_recur(int *array, int low, int heigh, size_t size)
 {
 	int index;
 
 	if (low < heigh)
 	{
 		index = partition(array, low, heigh, size);
-		quick_sort_recur(array, low, index-1, size);
+		quick_sort_recur(array, low, index - 1, size);
 		quick_sort_recur(array, index + 1, heigh, size);
 	}
 }
 /**
- * 
+ *partition - divides array into 2 parts
+ *@array: int array
+ * @low: int starting index
+ * @heigh: int end index
+ * @size: size of array
+ * Return: int index of pivot
 */
 int partition(int *array, int low, int heigh,  size_t size)
 {
@@ -43,13 +48,19 @@ int partition(int *array, int low, int heigh,  size_t size)
 	{
 		if (array[j] < pivot)
 		{
-			swap(&array[i], &array[j]);
-			print_array(array,size);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 			i++;
 		}
 	}
-	swap(&array[i], &array[heigh]);
-	print_array(array,size);
+	if (i != j)
+	{
+		swap(&array[i], &array[heigh]);
+		print_array(array, size);
+	}
 	return (i);
 }
 /**
