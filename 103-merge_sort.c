1fc38ee5cfef2e09391137dc3731c2b6db82f4d2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-25 00:19:50 +0200

    betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,9 @@
 #include "sort.h"
 /**
- * 
+ * merge_sort - sort array of int
+ * @array: array of int
+ * @size: size of array
+ * Return: Nothing
 */
 void merge_sort(int *array, size_t size)
 {
@@ -20,7 +23,12 @@ void merge_sort(int *array, size_t size)
 }
 
 /**
- * 
+ * merge_sort_reqer - sort array by merge sort (top -down)
+ * @array: array of int
+ * @buffer: tmp buffer to store sorted values
+ * @l: first index in left subarr
+ * @r: last index in right subarr
+ * Return: Nothing
 */
 void merge_sort_reqer(int *array, int *buffer, int l, int r)
 {
@@ -29,19 +37,26 @@ void merge_sort_reqer(int *array, int *buffer, int l, int r)
 	if (l < r)
 	{
 		mid = (l + r) / 2;
+		/*check on mid so left subarr_size <= right subarr_size*/
 		if ((l + r) % 2 == 0)
 		{
 			mid--;
 		}
 		merge_sort_reqer(array, buffer, l, mid);
-		merge_sort_reqer(array, buffer, mid + 1, r); 
+		merge_sort_reqer(array, buffer, mid + 1, r);
 		merge(array, buffer, l, mid, r);
 	}
 }
 /**
- * 
+ *  merge - merge sorted subarr into buffer
+ * @array: array of int
+ * @buffer: tmp buffer to store sorted values
+ * @left: first index in left subarr
+ * @mid: mid index of array
+ * @right: last index in right subarr
+ * Return: Nothing
 */
-void merge (int *array, int *buffer, int left, int mid, int right)
+void merge(int *array, int *buffer, int left, int mid, int right)
 {
 	int i, j, k = left;
 
@@ -49,9 +64,9 @@ void merge (int *array, int *buffer, int left, int mid, int right)
 	print_array(array + left, mid - left + 1);
 	printf("[right]: ");
 	print_array(array + mid + 1, right - mid);
-	for (i = left, j = mid + 1; i <= mid && j <=right; k++)
+	for (i = left, j = mid + 1; i <= mid && j <= right; k++)
 	{
-		if (array [i] <= array [j])
+		if (array[i] <= array[j])
 		{
 			buffer[k] = array[i++];
 		}
@@ -62,12 +77,12 @@ void merge (int *array, int *buffer, int left, int mid, int right)
 	}
 	/*transfer rest of values*/
 	while (i <= mid)
-	{ 
+	{
 		buffer[k++] = array[i++];
 	}
 
 	while (j <= right)
-	{ 
+	{
 		buffer[k++] = array[j++];
 	}
 	/*copying values into main arr*/
